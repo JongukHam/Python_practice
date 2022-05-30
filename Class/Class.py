f159c209b6cd941a5e9217d0eda5f10873728eff
JongukHam <hollywood44@naver.com>
2022-05-30 12:04:41 +0900

    Update Class.py
    오버라이딩

--- Class/Class.py
+++ Class/Class.py
@@ -123,6 +123,7 @@ myCar2=Car()
 myCar2.speed=60
 print("자동차1의 현재 속도는 %dkm, 생산된 자동차 숫자는 총 %d대입니다."%(myCar2.speed, Car.count))
 '''
+'''
 
 #클래스 상속
 
@@ -160,10 +161,41 @@ truck1.capacity=50
 
 print("승용차의 속도는 %d km, 좌석수는 %d개입니다."%(sedan1.speed,sedan1.getSeatNum()))
 print("트럭의 속도는 %d km, 총중량은 %d톤입니다."%(truck1.speed,truck1.getCapacity()))
+'''
+
+#오버라이딩
 
 
+class Car:
+    speed=0
 
+    def upSpeed(self,value):
+        self.speed = self.speed+value
 
+        print("현재 속도(슈퍼 클래스) : %d" %self.speed)
+
+class Sedan(Car) : 
+    def upSpeed(self,value):
+        self.speed +=value
+
+        if self.speed>150:
+            self.speed=150
+
+        print("현재 속도(서브 클래스) : %d" %self.speed)
+
+class Truck(Car):
+    pass
+
+sedan1,truck1 = None,None
+
+truck1=Truck()
+sedan1=Sedan()
+
+print("트럭 -->", end="")
+truck1.upSpeed(200)
+
+print("승용차 -->", end="")
+sedan1.upSpeed(200)
 
 
         
