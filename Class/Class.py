4154f4af2f27b950228cb9cf6f37aa98243011ff
JongukHam <hollywood44@naver.com>
2022-05-30 11:00:53 +0900

    Update Class.py
    매개변수가 있는 생성자 추가

--- Class/Class.py
+++ Class/Class.py
@@ -34,7 +34,7 @@ print("자동차2의 색상은 %s 이며, 현재속도는 %d km 입니다."%(myC
 myCar3.upSpeed(0)
 print("자동차3의 색상은 %s 이며, 현재속도는 %d km 입니다."%(myCar3.color,myCar3.speed))
 '''
-
+'''
 #매개변수가 없는 생성자 추가
 
 class Car:
@@ -69,4 +69,31 @@ print("자동차1의 색상은 %s 이며, 현재속도는 %d km 입니다."%(myC
 print("자동차2의 색상은 %s 이며, 현재속도는 %d km 입니다."%(myCar2.color,myCar2.speed))
 #자동차3만 파란색이된다.
 print("자동차3의 색상은 %s 이며, 현재속도는 %d km 입니다."%(myCar3.color,myCar3.speed))
+'''
+#매개변수가 있는 생성자 추가
+class Car:
+    color=""
+    speed=0
 
+    
+    #매개변수(value1,~2)가 있는 생성자
+    def __init__(self,value1,value2) :
+        self.color = value1
+        self.speed = value2
+
+
+#메인 코드 부분
+
+myCar1=Car("빨강",10)
+
+myCar2=Car("파랑",20)
+
+myCar3=Car("노랑",30)
+
+
+
+print("자동차1의 색상은 %s 이며, 현재속도는 %d km 입니다."%(myCar1.color,myCar1.speed))
+
+print("자동차2의 색상은 %s 이며, 현재속도는 %d km 입니다."%(myCar2.color,myCar2.speed))
+
+print("자동차3의 색상은 %s 이며, 현재속도는 %d km 입니다."%(myCar3.color,myCar3.speed))
